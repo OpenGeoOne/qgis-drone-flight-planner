6862ff822713fe47ec81bb772f1c53eab7b38ba2
Leandro França <geoleandro.franca@gmail.com>
2026-01-13 21:56:21 -0300

    Update PlanoVoo_H_Manual.py

--- algoritmos/PlanoVoo_H_Manual.py
+++ algoritmos/PlanoVoo_H_Manual.py
@@ -673,7 +673,7 @@ class PlanoVoo_H_Manual(QgsProcessingAlgorithm):
         return 'Flight_Plan_H_Manual'
 
     def displayName(self):
-        return self.tr('2.Following terrain - Manual')
+        return self.tr('2. Following terrain - Manual')
 
     def group(self):
         return 'Horizontal Flight'
@@ -696,16 +696,6 @@ class PlanoVoo_H_Manual(QgsProcessingAlgorithm):
     texto = """This tool enables drone flight planning for photogrammetry, following terrain elevations (optionally), and lateral and frontal overlaps are entered. And you can also choose the photo interval by <b>distance</b> or by <b>time</b>.</b><br>
 It generates <b>CSV</b> file compatible with the <b>Litchi app</b> and 2 Layers - <b>Flight Line</b> and <b>Photos Points</b>.
 <p>It can also be used with other flight applications, utilizing the 2 genereted Layers for flight lines and waypoints.</p>
-<p><b>Learn more:</b><o:p></o:p></p>
-<ul style="margin-top: 0cm;" type="disc">
-   <li><a href="https://geoone.com.br/pvplanodevoo">Sign up for GeoFlight Planner course</a><o:p></o:p></li>
-   <li><a href="https://portal.geoone.com.br/m/lessons/planodevoo?classId=5680">Click here to access the class with all the details about this tool!</a><o:p></o:p></li>
-</ul>
-<p><b>Tips:</b><o:p></o:p></p>
-<ul style="margin-top: 0cm;" type="disc">
-  <li><a href="https://geoone.com.br/plano-de-voo-para-drone-com-python/#sensor">Check your drone sensor parameters</a><o:p></o:p></li>
-  <li><a href="https://geoone.com.br/opentopography-qgis/">Obtain the MDE for the Open Topography plugin</a><o:p></o:p></span></li>
-</ul>
 """
 
     figura2 = 'images/Terrain_Follow.jpg'
@@ -715,6 +705,11 @@ It generates <b>CSV</b> file compatible with the <b>Litchi app</b> and 2 Layers 
                       <img src="'''+ os.path.join(os.path.dirname(os.path.dirname(__file__)), self.figura2) +'''">
                       </div>
                       <div align="right">
+                      <p><b>Learn more:</b><o:p></o:p></p>
+                        <ul style="margin-top: 0cm;" type="disc">
+                        <li><a href="https://geoone.com.br/pvplanodevoo">Sign up for GeoFlight Planner course</a><o:p></o:p></li>
+                        <li><a href="https://portal.geoone.com.br/m/lessons/planodevoo?classId=5680">Click here to access the class with all the details about this tool!</a><o:p></o:p></li>
+                        </ul>
                       <p align="right">
                       <b>Autores: Prof Cazaroli & Leandro França</b>
                       </p>
