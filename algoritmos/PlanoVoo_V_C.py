e8ad4a1c167cadfa235c50c589210c1dcd30716f
Leandro França <geoleandro.franca@gmail.com>
2026-01-13 21:56:08 -0300

    Update PlanoVoo_V_C.py

--- algoritmos/PlanoVoo_V_C.py
+++ algoritmos/PlanoVoo_V_C.py
@@ -400,7 +400,7 @@ class PlanoVoo_V_C(QgsProcessingAlgorithm):
         return 'Flight_Plan_V_C'
 
     def displayName(self):
-        return self.tr('2.Circular')
+        return self.tr('2. Circular')
 
     def group(self):
         return 'Vertical Flight'
@@ -430,17 +430,6 @@ It enables the creation of an optimized flight path to capture detailed images o
 </ul>
 <p><span>The outputs are <b>CSV</b> file compatible with the <b>Litchi app</b>. and 2 Layers - <b>Flight Line</b> and <b>Photos Points</b>.
 <p>It can also be used with other flight applications, utilizing the 2 genereted Layers for flight lines and waypoints.</p>
-<p>
-<p><b>Learn more:</b><o:p></o:p></p>
-<ul style="margin-top: 0cm;" type="disc">
-   <li><a href="https://geoone.com.br/pvplanodevoo">Sign up for GeoFlight Planner course</a><o:p></o:p></li>
-   <li><a href="https://portal.geoone.com.br/m/lessons/planodevoo?classId=5682">Click here to access the class with all the details about this tool!</a><o:p></o:p></li>
-</ul>
-<p><b>Tips:</b><o:p></o:p></p>
-<ul style="margin-top: 0cm;" type="disc">
-  <li><a href="https://geoone.com.br/plano-de-voo-para-drone-com-python/#sensor">Check your drone sensor parameters</a><o:p></o:p></li>
-  <li><a href="https://geoone.com.br/opentopography-qgis/">Obtain the MDE for the Open Topography plugin</a><o:p></o:p></span></li>
-</ul>
             """
 
     figura = 'images/Circular.jpg'
@@ -450,6 +439,11 @@ It enables the creation of an optimized flight path to capture detailed images o
                       <img src="'''+ os.path.join(os.path.dirname(os.path.dirname(__file__)), self.figura) +'''">
                       </div>
                       <div align="right">
+                      <p><b>Learn more:</b><o:p></o:p></p>
+                        <ul style="margin-top: 0cm;" type="disc">
+                        <li><a href="https://geoone.com.br/pvplanodevoo">Sign up for GeoFlight Planner course</a><o:p></o:p></li>
+                        <li><a href="https://portal.geoone.com.br/m/lessons/planodevoo?classId=5682">Click here to access the class with all the details about this tool!</a><o:p></o:p></li>
+                        </ul>
                       <p align="right">
                       <b>Autores: Prof Cazaroli & Leandro França</b>
                       </p>
