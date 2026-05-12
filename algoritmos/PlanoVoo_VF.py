6f7440e059b4781d92a9feafcd1b17b4fcf471f0
Leandro França <geoleandro.franca@gmail.com>
2026-05-12 19:30:23 -0300

    Update PlanoVoo_VF.py

--- algoritmos/PlanoVoo_VF.py
+++ algoritmos/PlanoVoo_VF.py
@@ -21,7 +21,6 @@ __revision__ = '$Format:%H$'
 from qgis.core import *
 from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtCore import QCoreApplication
-from ..images.Imgs import *
 import os
 import numpy as np
 
@@ -318,7 +317,9 @@ It generates an optimized vertical flight trajectory parallel to a reference fac
                       <p align="right">
                       <b>Autores: Prof Cazaroli & Leandro França</b>
                       </p>
-                      <a target="_blank" rel="noopener noreferrer" href="https://geoone.com.br/"><img title="GeoOne" src="data:image/png;base64,'''+ GeoOne +'''"></a>
+                      <a target="_blank" rel="noopener noreferrer" href="https://geoone.com.br/">
+                      <img title="GeoOne" width="280"  src="{os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images/GeoOne.png')}">
+                      </a>
 					  <p><i>"Automated, easy and straight to the point mapping is at GeoOne!"</i></p>
                       </div>
                     </div>'''
