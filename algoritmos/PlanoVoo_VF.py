200b374c76b3c088776cdfe87dcdc2aea0e13f02
Leandro França <geoleandro.franca@gmail.com>
2026-05-12 19:40:39 -0300

    Update PlanoVoo_VF.py

--- algoritmos/PlanoVoo_VF.py
+++ algoritmos/PlanoVoo_VF.py
@@ -306,7 +306,7 @@ It generates an optimized vertical flight trajectory parallel to a reference fac
 
     def shortHelpString(self):
         corpo = '''<div align="center">
-                      <img src="'''+ os.path.join(os.path.dirname(os.path.dirname(__file__)), self.figura) +'''">
+                      <img src="'''+ os.path.join(os.path.dirname(os.path.dirname(__file__)), self.figura) +f'''">
                       </div>
                       <div align="right">
                       <p><b>Learn more:</b><o:p></o:p></p>
