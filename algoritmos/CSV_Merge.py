212ca26dd50824ebd93967b53b4f723b4da10c8f
Leandro França <geoleandro.franca@gmail.com>
2026-05-21 17:44:12 -0300

    Update CSV_Merge.py

--- algoritmos/CSV_Merge.py
+++ algoritmos/CSV_Merge.py
@@ -242,9 +242,7 @@ class CSV_Merge(QgsProcessingAlgorithm):
                       <p align="right">
                       <b>Autores: Prof Cazaroli, Leandro França and Ilton Freitas</b>
                       </p>
-                      <a target="_blank" rel="noopener noreferrer" href="https://geoone.com.br/">
-                      <img title="GeoOne" width="280"  src="{os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images/GeoOne.png')}">
-                      </a>
+                      <a target="_blank" rel="noopener noreferrer" href="https://geoone.com.br/"><img title="GeoOne" width="280"  src="'''+ os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images/GeoOne.png') +'''"></a>
 					  <p><i>"Automated, easy and straight to the point mapping is at GeoOne!"</i></p>
                       </div>
                     </div>'''
