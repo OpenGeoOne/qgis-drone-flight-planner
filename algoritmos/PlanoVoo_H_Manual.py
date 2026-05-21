ae4b80e8aa12ef855bddfdff164dab05f36bcc63
Leandro França <geoleandro.franca@gmail.com>
2026-05-21 17:44:02 -0300

    Update PlanoVoo_H_Manual.py

--- algoritmos/PlanoVoo_H_Manual.py
+++ algoritmos/PlanoVoo_H_Manual.py
@@ -193,7 +193,7 @@ It generates <b>CSV</b> file compatible with the <b>Litchi app</b> and 2 Layers 
 
     def shortHelpString(self):
         corpo = '''<div align="center">
-                      <img src="'''+ os.path.join(os.path.dirname(os.path.dirname(__file__)), self.figura) +f'''">
+                      <img src="'''+ os.path.join(os.path.dirname(os.path.dirname(__file__)), self.figura) + '''">
                       </div>
                       <div align="right">
                       <p><b>Learn more:</b><o:p></o:p></p>
@@ -204,9 +204,7 @@ It generates <b>CSV</b> file compatible with the <b>Litchi app</b> and 2 Layers 
                       <p align="right">
                       <b>Autores: Prof Cazaroli & Leandro França</b>
                       </p>
-                      <a target="_blank" rel="noopener noreferrer" href="https://geoone.com.br/">
-                      <img title="GeoOne" width="280"  src="{os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images/GeoOne.png')}">
-                      </a>
+                        <a target="_blank" rel="noopener noreferrer" href="https://geoone.com.br/"><img title="GeoOne" width="280"  src="'''+ os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images/GeoOne.png') +'''"></a>
 					  <p><i>"Automated, easy and straight to the point mapping is at GeoOne!"</i></p>
                       </div>
                     </div>'''
